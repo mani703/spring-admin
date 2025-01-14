4eddc7b344de9d95641c97efd55a4fef10c84d7a
mani703 <mani703@naver.com>
2025-01-14 22:33:54 +0900

    service abstract

--- study/src/main/java/com/example/study/controller/api/OrderGroupApiController.java
+++ study/src/main/java/com/example/study/controller/api/OrderGroupApiController.java
@@ -2,6 +2,7 @@ package com.example.study.controller.api;
 
 import com.example.study.controller.CrudController;
 import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.OrderGroup;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.OrderGroupApiRequest;
 import com.example.study.model.network.response.OrderGroupApiResponse;
@@ -12,13 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orderGroup")
-public class OrderGroupApiController extends CrudController<OrderGroupApiRequest, OrderGroupApiResponse> {
+public class OrderGroupApiController extends CrudController<OrderGroupApiRequest, OrderGroupApiResponse, OrderGroup> {
 
-    @Autowired
-    private OrderGroupApiLogicService orderGroupApiLogicService;
-
-    @PostConstruct
-    public void init() {
-       this.baseService = orderGroupApiLogicService;
-   }
 }
